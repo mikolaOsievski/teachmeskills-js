26e9636c02b17e570d2bcc7250a9084ad1f5c0f7
nik Osievski <nikosievski@gmail.com>
2021-01-26 20:55:20 +0300

    Done homework-21

--- script.js
+++ script.js
@@ -10,6 +10,16 @@
 где result это аргумент resolve.
 */
 
+function cleanRoom(dirtyLevel) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+  });
+}
+
+cleanRoom(3).then((result) =>
+  console.log(`Уборка проведена успешно за ${result} секунд(ы)(у)`)
+);
+
 /* Task #2
 Расширьте предыдущее задание так, чтобы при передаче в dirtyLevel > 10 вы спустя dirtyLevel * 1000 мс
 возвращали выполненный с ошибкой промис.
@@ -17,6 +27,21 @@
 Для обработки неуспешной уборки в then используйте console.log(err). Где err это аргумент reject.
 Текст ошибки придумайте сами.
 */
+
+function cleanRoom(dirtyLevel) {
+  return new Promise((resolve, reject) => {
+    if (dirtyLevel > 10) {
+      reject("Совсем грязная комната!!! Я в сарае не убираю! ");
+    } else {
+      setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+    }
+  });
+}
+
+cleanRoom(2).then(
+  (result) => console.log(`Уборка проведена успешно за ${result} секунд(ы)(у)`),
+  (err) => console.log(err)
+);
 
 /* Task #3
 Задание со звездочкой.
@@ -29,6 +54,41 @@
 Для этого функция cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) { ... } должна принимать три аргумента,
 каждый из кторых описывает уровень загрязнения каждой комнаты.
 */
+
+function cleanRoomOne(dirtyLevel) {
+  return new Promise((resolve, reject) => {
+    if (dirtyLevel > 10) {
+      reject("Совсем грязная комната!!! Я в сарае не убираю! ");
+    } else {
+      setTimeout(() => resolve(dirtyLevel), dirtyLevel * 1000);
+    }
+  });
+}
+
+function cleanRoom(dirtyLevel_1, dirtyLevel_2, dirtyLevel_3) {
+  cleanRoomOne(dirtyLevel_1)
+    .then(() => {
+      console.log(
+        `Уборка комнаты #1 проведена успешно за ${dirtyLevel_1} секунд(ы)(у)`
+      );
+      return cleanRoomOne(dirtyLevel_2);
+    })
+    .then(() => {
+      console.log(
+        `Уборка комнаты #2 проведена успешно за ${dirtyLevel_2} секунд(ы)(у)`
+      );
+      return cleanRoomOne(dirtyLevel_3);
+    })
+    .then(() => {
+      console.log(
+        `Уборка комнаты #3 проведена успешно за ${dirtyLevel_3} секунд(ы)(у)`
+      );
+      console.log(`Все комнаты убраны!`);
+    })
+    .catch((err) => console.log(err));
+}
+
+cleanRoom(2, 11, 6);
 
 /* Task #4
 Задача на повторение массивов.
@@ -47,3 +107,34 @@
 
 Обратите внимание что информация должна быть отсортирована по убыванию повторений.
 */
+
+let arr = [
+  "str",
+  "str1",
+  "str2",
+  "str",
+  "str2",
+  "str",
+  "str3",
+  "str1",
+  "str4",
+  "str4",
+];
+
+function countRepeats(array) {
+  let obj = {};
+
+  array.forEach((item) => {
+    obj[item] = (obj[item] || 0) + 1;
+  });
+
+  let arrSorted = Object.entries(obj).sort(function (a, b) {
+    return b[1] - a[1];
+  });
+
+  let objSorted = {};
+  arrSorted.forEach((item) => (objSorted[item[0]] = item[1]));
+  return objSorted;
+}
+
+console.log(countRepeats(arr));
